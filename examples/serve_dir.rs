1915160d927776ad096a0f4b23596886bd999e8d
Mendelt Siebenga <mendelt@mendelt.nl>
2021-02-20 14:15:27 +0100

    finish the examples

--- examples/serve_dir.rs
+++ examples/serve_dir.rs
@@ -1,7 +1,10 @@
 use tide_fs::prelude::*;
 
-fn main() -> std::io::Result<()> {
+#[async_std::main]
+async fn main() -> std::io::Result<()> {
     let mut app = tide::new();
     app.at("static/css/").serve_dir("static_content/css/")?;
+    app.listen("127.0.0.1:8080").await?;
+
     Ok(())
 }
--- examples/serve_file.rs
+++ examples/serve_file.rs
@@ -1,7 +1,10 @@
 use tide_fs::prelude::*;
 
-fn main() -> std::io::Result<()> {
+#[async_std::main]
+async fn main() -> std::io::Result<()> {
     let mut app = tide::new();
     app.at("index.html").serve_file("files/index.html")?;
+    app.listen("127.0.0.1:8080").await?;
+
     Ok(())
 }
